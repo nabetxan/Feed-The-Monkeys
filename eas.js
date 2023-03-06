8e9cdd5cad6466cb5396102be0e86d5b497e4ee9
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-03-06 11:22:31 +0900

    fear: add scale bar for size change

--- eas.js
+++ eas.js
@@ -1,11 +1,38 @@
-// document.addEventListener("DOMContentLoaded", function(event) {
-const rowSetting = 5;
+let rowSetting = 5;
 const container = document.getElementById("container");
 
+// input要素
+const divCountScale = document.getElementById("divCountScale");
+// input用Scaleから取得する値
+const currentDivCountValue = divCountScale.value;
+// currentValue表示するDiv
+const divCountDisplay = document.getElementById("divCountDisplay");
+// // divCountDisplayにcurrentDivCountValueを表示する
+// divCountDisplay.textContent = currentDivCountValue;
+
+// divCountDisplayにcurrentDivCountValueを表示する
+const setCurrentValue = (val) => {
+  divCountDisplay.textContent = val;
+};
+
+// inputイベント時に値をセットする関数
+const rangeOnChange = (e) => {
+  setCurrentValue(e.target.value);
+};
+window.onload = () => {
+  divCountScale.addEventListener("input", rangeOnChange);
+  // ページ読み込み時の値をセット
+  setCurrentValue(divCountScale.value);
+  // createDiv(divCountScale.value);
+};
+
 function createDiv() {
+  container.textContent = "";
+  rowSetting = divCountDisplay.outerText;
   container.style.setProperty("--get-row", rowSetting);
   for (i = 1; i <= rowSetting * rowSetting; i++) {
     const div = document.createElement("div");
+    div.classList.add("canvas");
     div.textContent = "🐒";
     container.appendChild(div);
   }
@@ -13,4 +40,4 @@ function createDiv() {
 
 const button = document.getElementById("buttonToCreateDivs");
 button.addEventListener("click", createDiv);
-// });
+// button.addEventListener("click", createDiv, {once: true,});
